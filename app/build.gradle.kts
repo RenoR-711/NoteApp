21bc240091e7d4088aec8a7c3347f78a3d8ed313
RenoR <renu711@hotmail.de>
2025-10-26 16:19:03 +0100

    build.gradle.kts+Dao

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.core.splashscreen)
 
     // Lifecycle (Views)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
